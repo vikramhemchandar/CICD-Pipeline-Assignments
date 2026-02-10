50594568c37f100905283aa63bd3f8a2aeece66d
Vikram Hem Chandar <vikramhemchandar@gmail.com>
2026-02-10 23:53:51 +0530

    WIP: Fixed Jenkinsfile - 23

--- app.py
+++ app.py
@@ -3,17 +3,16 @@ from flask import Flask, jsonify
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
+    @app.route('/')
     def home():
-        return "Hello Flask", 200
+        return jsonify({"message": "Hello, Jenkins CI/CD!"})
 
-    @app.route("/health")
+    @app.route('/health')
     def health():
-        return jsonify({"status": "ok"}), 200
-
+        return jsonify({"status": "healthy"}), 200
+    
     return app
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = create_app()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
